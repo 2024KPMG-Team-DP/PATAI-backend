1801f0624c3c1e684084c6602eefce2e4ce5d224
y-ngm-n <wogitopp@naver.com>
2024-02-08 01:46:29 +0900

    [FEAT] POST /techReview: 1번 기능 임시 구현 완료

---
+++ src/app.js
@@ -0,0 +1,42 @@
+// import modules
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+// import routes
+const techReviewsRouter = require("./routes/techReviews");
+
+
+// config
+dotenv.config();
+
+// create app
+const app = express();
+
+// app setting
+app.set("port", 8000);
+
+// middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routers
+app.use("/techReview", techReviewsRouter);
+
+// error handling routers
+app.use((req, res) => {
+  const err = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  res.status(404).json(err);
+});
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json(err);
+})
+
+
+// start app
+app.listen(app.get("port"), () => {
+  console.log("Server running,,,");
+});
