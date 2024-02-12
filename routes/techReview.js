049613a622c65ee58f54cfcf786751ff051eccc9
y-ngm-n <wogitopp@naver.com>
2024-02-12 17:53:39 +0900

    [FEAT] Output PDF 처리: 객체 형식 Input 데이터

--- routes/techReview.js
+++ routes/techReview.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 
 // import files
 const { techPrompt, lawPrompt } = require("../prompts/techReviewPrompt");
-const data = require("./data.json");
+// const data = require("./data.json");
 
 // config
 dotenv.config();
@@ -61,53 +61,119 @@ const queryToLawIndex = async (techResponse) => {
   return result;
 }
 
-// // 답변 생성
-// const generateAnswer = async (userPrompt) => {
-//   try {
-//     // 선행기술 DB 탐색 결과
-//     const techReviewResult = await queryToTechIndex(userPrompt);
+// 답변 생성
+const generateAnswer = async (userPrompt) => {
+  try {
+    // 선행기술 DB 탐색 결과
+    const techReviewResult = await queryToTechIndex(userPrompt);
 
-//     // 대화 생성
-//     const dialogue = [
-//       {
-//         role: "system",
-//         content: techPrompt
-//         + JSON.stringify(techReviewResult.matches[0].metadata)
-//         + JSON.stringify(techReviewResult.matches[1].metadata)
-//         + JSON.stringify(techReviewResult.matches[2].metadata)
-//         + JSON.stringify(techReviewResult.matches[3].metadata)
-//         + JSON.stringify(techReviewResult.matches[4].metadata)
-//       },
-//       { role: "system", content: lawPrompt },
-//       { role: "user", content: userPrompt }
-//     ];
+    // 대화 생성
+    const dialogue = [
+      {
+        role: "system",
+        content: techPrompt
+        + JSON.stringify(techReviewResult.matches[0].metadata)
+        + JSON.stringify(techReviewResult.matches[1].metadata)
+        + JSON.stringify(techReviewResult.matches[2].metadata)
+        + JSON.stringify(techReviewResult.matches[3].metadata)
+        + JSON.stringify(techReviewResult.matches[4].metadata)
+      },
+      { role: "system", content: lawPrompt },
+      { role: "user", content: userPrompt }
+    ];
 
-//     // 선행기술 검토 답변
-//     const techResponse = await client.getChatCompletions(process.env.AZURE_GPT, dialogue);
-//     console.log(techResponse.choices[0].message);
+    // 선행기술 검토 답변
+    const techResponse = await client.getChatCompletions(process.env.AZURE_GPT, dialogue);
+    console.log(techResponse.choices[0].message);
 
-//     // 특허법 DB 탐색 결과
-//     const lawReviewResult = await queryToLawIndex(techResponse.choices[0].message.content);
+    // 특허법 DB 탐색 결과
+    const lawReviewResult = await queryToLawIndex(techResponse.choices[0].message.content);
 
-//     // 대화 추가
-//     dialogue.push(techResponse.choices[0].message);
-//     dialogue.push({
-//       role: "system",
-//       content: lawPrompt
-//       + JSON.stringify(lawReviewResult.matches[0].metadata)
-//       + JSON.stringify(lawReviewResult.matches[1].metadata)
-//       + JSON.stringify(lawReviewResult.matches[2].metadata)
-//     });
+    // 대화 추가
+    dialogue.push(techResponse.choices[0].message);
+    dialogue.push({
+      role: "system",
+      content: lawPrompt
+      + JSON.stringify(lawReviewResult.matches[0].metadata)
+      + JSON.stringify(lawReviewResult.matches[1].metadata)
+      + JSON.stringify(lawReviewResult.matches[2].metadata)
+    });
 
-//     // 특허법 검토 답변
-//     const lawResponse = await client.getChatCompletions(process.env.AZURE_GPT, dialogue);
-//     return lawResponse.choices[0].message;
-//   } catch (err) { console.error(err); }
-// }
+    // 특허법 검토 답변
+    const lawResponse = await client.getChatCompletions(process.env.AZURE_GPT, dialogue);
+    return lawResponse.choices[0].message;
+  } catch (err) { console.error(err); }
+}
 
 // 보고서 생성
-const generateReport = async (userPrompt) => {
+const generateReport = async (body, userPrompt) => {
   try {
+    // 선행기술 DB 탐색 결과
+    const techReviewSearchResult = await queryToTechIndex(userPrompt);
+
+    // 대화 생성
+    const dialogue = [
+      {
+        role: "system",
+        content: techPrompt
+        + JSON.stringify(techReviewSearchResult.matches[0].metadata)
+        + JSON.stringify(techReviewSearchResult.matches[1].metadata)
+        + JSON.stringify(techReviewSearchResult.matches[2].metadata)
+        // + JSON.stringify(techReviewSearchResult.matches[3].metadata)
+        // + JSON.stringify(techReviewSearchResult.matches[4].metadata)
+      },
+      { role: "system", content: lawPrompt },
+      { role: "user", content: userPrompt }
+    ];
+
+    // 답변
+    const response = await client.getChatCompletions(process.env.AZURE_GPT, dialogue);
+    const date = new Date();
+
+    // 보고서 항목
+    const data = {
+      info: {
+        registration: "",
+        registerDate: body.date,
+        company: body.organization,
+        nowDate: `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDate()}일`,
+        name: body.name,
+        report: "등록가능성 진단보고서",
+        summary: body.description
+      },
+      result: {
+        otherPatents: [
+          {
+            index: techReviewSearchResult.matches[0].id,
+            registration: techReviewSearchResult.matches[0].metadata.registration,
+            registerDate: "",
+            company: "",
+            name: techReviewSearchResult.matches[0].metadata.name,
+            similarity: ""
+          },
+          {
+            index: techReviewSearchResult.matches[1].id,
+            registration: techReviewSearchResult.matches[1].metadata.registration,
+            registerDate: "",
+            company: "",
+            name: techReviewSearchResult.matches[1].metadata.name,
+            similarity: ""
+          },
+          {
+            index: techReviewSearchResult.matches[2].id,
+            registration: techReviewSearchResult.matches[2].metadata.registration,
+            registerDate: "",
+            company: "",
+            name: techReviewSearchResult.matches[2].metadata.name,
+            similarity: ""
+          },
+        ],
+        opinion: response.choices[0].message.content,
+        probability: ""
+      }
+    }
+
+    // 보고서 생성
     const document = {
       html: reportTemplate,
       data: { info: data.info, result: data.result },
@@ -124,7 +190,7 @@ const generateReport = async (userPrompt) => {
 router.post("/", async (req, res) => {
   const { body } = req;
   const userPrompt = JSON.stringify(body);
-  const result = await generateReport(userPrompt);
+  const result = await generateReport(body, userPrompt);
   res.setHeader("Content-Type", "application/pdf");
   res.setHeader("Content-Disposition", "attachment; filename=report.pdf");
   res.send(result);
